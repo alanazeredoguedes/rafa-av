9cf7ee2231a6ea72c8f0af95993d57e815705f66
Alan Guedes <alanguedes00@live.com>
2021-10-01 21:41:28 -0300

    add tradução

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 /** ================================================================== */
 const FileHelper = require('./src/classes/FileHelper')
 const liwc = require('./src/lib/liwc.js');
+const liwc_br = require('./src/lib/liwc_br.js');
 const {js} = require("blade/lib/filters");
 //const googleTranslate = require('google-translate')(apiKey, options);
 
@@ -15,187 +16,94 @@ let file_helper = new FileHelper()
 /** ============================ Run Code ============================ */
 /** ================================================================== */
 
-let info = liwc.fromText("anxious");
-
-let feelings = [];
-for (let i in info) {
-    if (info[i] === 1) {
-        feelings.push(i);
-    }
-}
-
-console.log(info)
-console.log(feelings)
-
-
-
 /*
-let dicionario = file_helper.get_file('translate/dicionario_br.json')
-dicionario = JSON.parse(dicionario);
-
-console.log(dicionario)
+let busca = liwc_br.busca_individual('nao')
+console.log(busca)
 
 */
 
 
+let frase = file_helper.get_file('frases/frase.txt')
 
+//let frase = 'amo muito jurandina jurandina VUCUVUCU'
 
+let fraseArray = filter_frase(frase)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let referencia = file_helper.get_file('translate/dicionario_pt_br.txt')
-
-//console.log(referencia)
-
-while (referencia.includes('\n')){
-    referencia = referencia.replace('\n',' ');
+let resposta = {
+    swear: 0,
+    anx: 0,
+    posemo: 0,
+    negemo: 0,
+    notFound: 0,
+    notFoundWords: {}
 }
 
-let referenciaArray = referencia.split(' ')
-referenciaArray.pop()
-
-let json_arr = {};
-
-referenciaArray.forEach(function (data){
-    data = data.split('\t')
-    let values = []
-    data.forEach(function (valores,index){
-        if(index !== 0){
-            values.push(valores)
-        }
-    })
-    json_arr[data[0]] = values;
-});
-
-//json_arr.pop()
-
-let json_string = JSON.stringify(json_arr);
-
-//console.log(json_string)
-
-file_helper.put_file('translate/dicionario_br.json', json_string)
-*/
-
-//
-//
-// let json_arr = {};
-
-// referenciaArray.forEach(function (data){
-//     data = data.split('\t')
-//     json_arr[data[0]] = data[1];
-// });
-//
-// let json_string = JSON.stringify(json_arr);
-//
-// console.log(json_string)
-
-//file_helper.put_file('translate/.json', json_string)
-
-//
-// referencia_dados.pop()
-//
-// var json_arr = {};
-// json_arr[] = referencia_dados
-//
-// let myJsonString = JSON.parse(referencia_dados)
-//
-//
-// console.log(myJsonString)
-//
-//
-// file_helper.put_file('translate/referencia.json', myJsonString)
-
-
-//console.log(referenciaArray).split('\t')
-
-
-
-
-// let frase = file_helper.get_file('frases/frase.txt')
-// console.log(frase)
-//
-// while (frase.includes('\n')){
-//     frase = frase.replace('\n',' ');
-// }
-//
-// while (frase.includes(',')){
-//     frase = frase.replace(',',' ');
-// }
-//
-// let fraseArray = frase.split(' ')
-//
 // console.log(fraseArray)
 
+fraseArray.forEach((palavra)=> {
 
-/*let fraseArray = frase.split(' ')
+    let emocoes = liwc_br.busca_individual(palavra)
 
-console.log(fraseArray)*/
+    // console.log(palavra)
+    // console.log(emocoes)
 
+    if(emocoes.length){
 
-// googleTranslate.translate('My name is Alan', 'pt_br', function(err, translation) {
-//     console.log(translation.translatedText);
-//     // =>  Mi nombre es Brandon
-// });
+        emocoes.forEach((emocao)=>{
+            if(typeof resposta[emocao] !== 'undefined'){
+                resposta[emocao] += 1
+            }
+        })
 
-
-
-
-
-
-
-
-/*
-let frase = file_helper.get_file('translate/referencia.txt')
-console.log(frase)
-while (frase.includes('\n')){
-    frase = frase.replace('\n',' | ');
-}
-
-//frase = frase.replace(/ /g,'')
-
-console.log(frase)*/
-
-
-//arr = frase.split('|')
-
-
-//console.log(arr)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let info = liwc.fromText("anxious");
-
-let feelings = [];
-for (let i in info) {
-    if (info[i] === 1) {
-        feelings.push(i);
+    }else{
+        resposta['notFound'] += 1
+        if(typeof resposta['notFoundWords'][palavra] === 'undefined'){
+            resposta['notFoundWords'][palavra] = 1
+        }else{
+            resposta['notFoundWords'][palavra] += 1
+        }
     }
+
+})
+
+ console.log(resposta)
+
+
+
+
+
+/** ================================================================== */
+/** ============================ Functions =========================== */
+/** ================================================================== */
+
+function filter_frase(frase){
+
+    while(frase.includes('\n')){
+        frase = frase.replace('\n',' ');
+    }
+
+    while (frase.includes(',')){
+        frase = frase.replace(',',' ');
+    }
+    frase = frase.split(' ')
+
+    let array_de_palavras = []
+
+    frase.forEach((palavra, index, array)=>{
+        if(palavra){
+
+            palavra = tratar_caracteres_especiais(palavra)
+
+            array_de_palavras.push(palavra.toLowerCase())
+        }
+    })
+
+    return array_de_palavras
 }
 
-console.log(info)
-console.log(feelings)*/
+function tratar_caracteres_especiais(palavra){
 
+    //return  palavra.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '')
+    return  palavra.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '')
+
+}
---
+++ src/lib/liwc_br.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+let referenciaPath = './src/lib/translate/referencia.json'
+let idiomaPath = './src/lib/translate/dicionario_br.json'
+
+const referencia = JSON.parse( fs.readFileSync( referenciaPath, 'utf8') );
+
+const idioma = JSON.parse( fs.readFileSync( idiomaPath, 'utf8') );
+
+function busca_individual(busca){
+
+    let data = []
+
+    for (let key in idioma) {
+        if(tratar_caracteres_especiais(key) === busca){
+            //console.log(key)
+            data = idioma[key]
+        }
+    }
+
+    let sentimentos = []
+
+    if(data){
+        data.forEach((element) => {
+            sentimentos.push(referencia[element])
+            //sentimentos.push(referencia[element+'*'])
+        });
+    }
+
+    return sentimentos
+}
+
+function tratar_caracteres_especiais(palavra){
+
+    return  palavra.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '')
+
+}
+
+module.exports = {
+    busca_individual: busca_individual,
+};
+
+//console.log( busca_individual('merda') )
