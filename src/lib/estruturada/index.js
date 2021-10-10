0205807da0a8e67c54c2bd8e70214328de2ca46c
Ronaldo Guimarães <ronaldoguimar@live.com>
2021-10-10 16:51:20 -0300

    feature: Adição da implementação de forma estruturada

---
+++ src/lib/estruturada/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const liwcFunctions = require('./funcs')
+const fs = require('fs');
+
+// Le o arquivo com o texto a ser analisado
+let loadFile = fs.readFileSync('src/frases/frase.txt', 'utf-8')
+
+// Separa cada palavra do texto
+let palavrasSeparadas = liwcFunctions.separarPalavras(loadFile)
+//console.log(palavrasSeparadas)
+
+// Trata(tokezina?) cada palavra do texto, deixando tudo em minúsculo e sem acento.
+for (let i = 0; i < palavrasSeparadas.length; i++) {
+    palavrasSeparadas[i] = liwcFunctions.tratarPalavra(palavrasSeparadas[i]);
+}
+//console.log(palavrasSeparadas)
+
+const retornoAnaliseTexto = liwcFunctions.calcularTokensTexto(palavrasSeparadas)
+
+console.log(liwcFunctions.gerarSaidaFinal(retornoAnaliseTexto))
